b2d34c6fc6276884f8f0e760381130afadc91a80
Luca Scott <lucascott11@hotmail.com>
2025-11-28 03:42:28 +0000

    ChangedDirectory

--- Cantor-College/js/main.js
+++
@@ -1,8 +0,0 @@
-const burger = document.getElementById("burger");
-const nav = document.querySelector(".nav-links");
-
-burger.addEventListener("click", () => {
-    nav.style.display = nav.style.display === "flex" ? "none" : "flex";
-    burger.classList.toggle("active");
-});
-
